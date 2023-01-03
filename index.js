c9f1f03e434919eb671b06b0f637feb2ed14e0ad
Micah <80253494+coderMicah@users.noreply.github.com>
2023-01-03 15:33:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("openBtn").addEventListener("click", (e) => {
+  document.getElementById("menu").classList.toggle("hidden")
+});
+document.getElementById("closeBtn").addEventListener("click", (e) => {
+  document.getElementById("menu").classList.toggle("hidden")
+});
